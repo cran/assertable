04f43fd79fb0a0970f7293f34ec29864ecbcf6d0
Grant Nguyen <gngu@uw.edu>
2017-01-27 09:38:06 +0000

    version 0.2.0

--- R/assert_ids.R
+++ R/assert_ids.R
@@ -10,16 +10,16 @@
 #' Note: if assert_combos = T and is violated, then assert_ids will stop execution and return results for assert_combos
 #'        before evaluating the assert_dups segment of the code. 
 #'        If you want to make sure both options are evaluated even in case of a violation in assert_combos,
-#'        call assert_ids twice (once with assert_dups = F, then assert_combos = F) with warn_only = F, 
+#'        call assert_ids twice (once with assert_dups = F, then assert_combos = F) with warn_only = T, 
 #'        and then conditionally stop your code if either call returns results. 
 
 #' @param data A data.frame or data.table
-#' @param id_vars A named list of character vectors, where the name of each character vector must correspond to a column in \emph{data}
+#' @param id_vars A named list of vectors, where the name of each vector must correspond to a column in \emph{data}
 #' @param assert_combos Assert that the data object must contain all combinations of \emph{id_vars}. Default = T.
 #' @param assert_dups Assert that the data object must not contain duplicate values within any combinations of \emph{id_vars}. Default = T.
 #' @param ids_only By default, with assert_dups = T, the function returns the unique combinations of id_vars that have duplicate observations. 
 #'                  If ids_only = F, will return every observation in the original dataset that are duplicates.
-#' @param warn_only Do you want to warn, rather than error? Will return all offending rows from the first violation of the assertion Default=F
+#' @param warn_only Do you want to warn, rather than error? Will return all offending rows from the first violation of the assertion. Default=F
 
 #' @return Throws error if test is violated. Will print the offending rows. If warn_only=T, will return all offending rows and only warn.
 #' @export
@@ -39,7 +39,26 @@ assert_ids <- function(data, id_vars, assert_combos=TRUE, assert_dups=TRUE, ids_
     if(!varname %in% colnames(data)) stop(paste("The following id_var must exist as a column name in your dataset:",varname))
     ## Coerce factors to characters to avoid any merge or display issues
     if(is.factor(id_vars[[varname]])) id_vars[[varname]] <- as.character(id_vars[[varname]])
+    
+    ## Stop if trying to use assert_ids on a factor that contains underlying numeric values
+    ## Too difficult to match them (data.table matches numerics to the underlying numeric value, not the label)
+    ## And don't want to coerce the character types back-and-forth
+    if(is.data.table(data)){
+      if(is.factor(data[, get(varname)])) {
+        if(typeof(id_vars[[varname]]) != "character")
+          stop(paste0("Cannot evaluate ", varname, ": the variable in the data is factor while the values for the variable in the id_vars list are not character values or factors. ",
+                      "Either convert variable from factor to character/numeric or express id_vars as character values of the levels or as a factor itself"))
+      }
+    } else {
+      if(is.factor(data[, varname])){
+        if(typeof(id_vars[[varname]]) != "character")
+          stop(paste0("Cannot evaluate ", varname, ": the variable in the data is factor while the values for the variable in the id_vars list are not character values or factors. ",
+                      "Either convert variable from factor to character/numeric or express id_vars as character values of the levels or as a factor itself"))
+      }
+    }
   }
+
+  ## Extract the id variables from the dataset
   if(is.data.table(data)) {
     data_ids <- unique(data[, .SD, .SDcols=id_varnames])
   } else {
@@ -47,9 +66,9 @@ assert_ids <- function(data, id_vars, assert_combos=TRUE, assert_dups=TRUE, ids_
     if(length(id_varnames) == 1) {
       if(colnames(data_ids) == "V1") setnames(data_ids, "V1", id_varnames)
     }
-  }
+  }  
   
-  target_ids <- data.table(expand.grid(id_vars))
+  target_ids <- data.table(expand.grid(id_vars,stringsAsFactors=FALSE))
   if(assert_combos==TRUE){
     target_ids$in_target <- 1
     data_ids$in_data <- 1
@@ -109,4 +128,5 @@ assert_ids <- function(data, id_vars, assert_combos=TRUE, assert_dups=TRUE, ids_
       return(duplicate_ids)
     }
   }
+  print(paste0("Data is identified by id_vars: ",paste(id_varnames,collapse=" ")))
 }
